4b164d0af2b093f810a3eabb5d31b254f046931c
ozan40 <58996323+ozan40@users.noreply.github.com>
2025-11-08 08:20:38 +0100

    define try except block for request

--- infrastructure/lambda_ec2_orchestrator.py
+++ infrastructure/lambda_ec2_orchestrator.py
@@ -10,3 +10,13 @@ def lambda_handler(event, context):
 
     instance_id = "i-09fb9f484e58e382e"
     elastic_ip = "18.159.25.178"
+
+    try:
+        pass
+
+    except Exception as e:
+        print(f"Error in Lambda function: {str(e)}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }

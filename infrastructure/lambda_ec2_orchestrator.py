c52a531729424f27810c4498de782314023edf3a
ozan40 <58996323+ozan40@users.noreply.github.com>
2025-11-08 08:19:09 +0100

    initiate lambda_handler

--- infrastructure/lambda_ec2_orchestrator.py
+++ infrastructure/lambda_ec2_orchestrator.py
@@ -1,2 +1,10 @@
 import boto3
 import json
+
+def lambda_handler(event, context):
+    """
+    Simplified Lambda - just starts the instance, Systemd handles the rest
+    """
+
+    ec2 = boto3.client('ec2')
+    
